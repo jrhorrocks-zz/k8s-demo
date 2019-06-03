d294a4405ecf23d349af47059db9e656676d16cc
Justin Horrocks <jrhorrocks@gmail.com>
2019-06-03 10:37:07 -0700

    added host to page

--- web/app.py
+++ web/app.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return render_template("hello.html")
+    return render_template("hello.html", name=request.host)
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
