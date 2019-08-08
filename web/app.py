1a9f25f2d354288a3b6f63ff71cb7938b0e1e751
jrhorrocks <jrhorrocks@gmail.com>
2019-08-08 16:52:03 -0700

    updated host and ip

--- web/app.py
+++ web/app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, request
 
+import socket
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return render_template("hello.html", name=request.host)
+    return render_template("hello.html", name=socket.gethostname(), ip=socket.gethostbyname(socket.gethostname()))
 
 
 if __name__ == '__main__':
